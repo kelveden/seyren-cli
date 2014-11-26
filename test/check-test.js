a4dfdbac0d1166e901d13a6d4eec2f02f3daff47
Alistair Dutton <kelveden@gmail.com>
2014-11-26 11:14:55 +0000

    Add last warning/error to check output.

--- test/check-test.js
+++ test/check-test.js
@@ -8,35 +8,37 @@ var chai = require('chai'),
     Seyren = require('../lib/api');
 
 var dummyCheck = { id: '5464d353e4b026d842a579a2',
-    name: 'prod-mixradweb-load',
-    description: null,
-    target: 'sortByName(aliasByNode(prod.mixradweb.*.info.sys.load.normalized.05_mins_avg,2))',
-    from: null,
-    until: null,
-    enabled: true,
-    live: false,
-    state: 'OK',
-    subscriptions:
-        [ { id: '5464de8ce4b026d842a579c6',
-            target: '591675',
-            type: 'HUBOT',
-            su: true,
-            mo: true,
-            tu: true,
-            we: true,
-            th: true,
-            fr: true,
-            sa: true,
-            ignoreWarn: false,
-            ignoreError: false,
-            ignoreOk: false,
-            enabled: true,
-            fromTime: '0000',
-            toTime: '2359' } ],
-    warn: '2.5',
-    error: '3.0',
-    lastCheck: 1416498005058
-};
+        name: 'prod-mixradweb-load',
+        description: null,
+        target: 'sortByName(aliasByNode(prod.mixradweb.*.info.sys.load.normalized.05_mins_avg,2))',
+        from: null,
+        until: null,
+        enabled: true,
+        live: false,
+        state: 'OK',
+        subscriptions: [
+            { id: '5464de8ce4b026d842a579c6',
+                target: '591675',
+                type: 'HUBOT',
+                su: true,
+                mo: true,
+                tu: true,
+                we: true,
+                th: true,
+                fr: true,
+                sa: true,
+                ignoreWarn: false,
+                ignoreError: false,
+                ignoreOk: false,
+                enabled: true,
+                fromTime: '0000',
+                toTime: '2359' }
+        ],
+        warn: '2.5',
+        error: '3.0',
+        lastCheck: 1416498005058
+    },
+    dummyAlerts = { values: [] };
 
 describe("check command", function () {
     var config = {
@@ -64,11 +66,19 @@ describe("check command", function () {
                 milli.onGet('/api/checks/myid')
                     .respondWith(200)
                     .body(dummyCheck)
-                    .contentType("application/json")))
-
+                    .contentType("application/json")),
+            milli.expectRequest(
+                milli.onGet('/api/checks/myid/alerts')
+                    .respondWith(200)
+                    .body(dummyAlerts)
+                    .contentType("application/json")
+            )
+        )
             .run(function () {
                 check.execute(seyren, "myid")
-                    .done(function () { done(); }, done);
+                    .done(function () {
+                        done();
+                    }, done);
             });
     });
 });
